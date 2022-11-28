92d02f1aa17ae9226cf9ab75c9f92d4c2bc4a80e
Johannes Schindelin <johannes.schindelin@gmx.de>
2022-11-28 16:24:54 +0100

    Add Javascript code to issue GitHub API requests on behalf of a GitHub App
    We registered a GitHub App for the specific purpose of performing more
    holistic operations than the `GITHUB_TOKEN` of regular GitHub workflows
    offer us, specifically we want to be able to perform cross-repository
    check runs.
    
    Let's offer the base function to act as said GitHub App.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

---
+++ github-api-request-as-app.js
@@ -0,0 +1,41 @@
+module.exports = async (context, appId, privateKey, requestMethod, requestPath, body) => {
+    const header = {
+        "alg": "RS256",
+        "typ": "JWT"
+    }
+
+    const now = Math.floor(new Date().getTime() / 1000)
+
+    const payload = {
+        // issued at time, 60 seconds in the past to allow for clock drift
+        iat: now - 60,
+        // JWT expiration time (10 minute maximum)
+        exp: now + (10 * 60),
+        // GitHub App's identifier
+        iss: appId
+    }
+
+    const toBase64 = (obj) => Buffer.from(JSON.stringify(obj), "utf-8").toString("base64url")
+	const headerAndPayload = `${toBase64(header)}.${toBase64(payload)}`
+
+    const crypto = require('crypto')
+    const signer = crypto.createSign("RSA-SHA256")
+    signer.update(headerAndPayload)
+    const signature = signer.sign({
+        key: privateKey
+    }, "base64url")
+
+    const token = `${headerAndPayload}.${signature}`
+
+    const httpsRequest = require('./https-request')
+    return await httpsRequest(
+        context,
+        null,
+        requestMethod,
+        requestPath,
+        body,
+        {
+            Authorization: `Bearer ${token}`,
+        }
+    )
+}
