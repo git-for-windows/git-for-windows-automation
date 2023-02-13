36a514b8e95d105276558dbe107ceb6effdbf516
Johannes Schindelin <johannes.schindelin@gmx.de>
2023-02-13 08:23:56 +0100

    get-installation-access-token: return a struct, not just the token
    We recently ran into an expiring installation access token. To remedy
    that, we will want to return not only the token but also its expiry
    date. Prepare for that.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- get-installation-access-token.js
+++ get-installation-access-token.js
@@ -7,6 +7,8 @@ module.exports = async (context, appId, privateKey, installation_id) => {
         'POST',
         `/app/installations/${installation_id}/access_tokens`)
     if (answer.error) throw answer.error
-    if (answer.token) return answer.token
+    if (answer.token) return {
+        token: answer.token
+    }
     throw new Error(`Unhandled response:\n${JSON.stringify(answer, null, 2)}`)
 }
