5108544d4eab40cdf69e47a2171f3645178413d7
Johannes Schindelin <johannes.schindelin@gmx.de>
2023-02-28 17:05:31 +0100

    tag-git: refactor the script to prepare for tagging full versions
    The `tag-git` workflow can currently only tag snapshot versions.
    
    Let's disentangle that logic and put order into it, to prepare for
    optionally tagging full Git for Windows versions instead.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- update-scripts/tag-git.sh
+++ update-scripts/tag-git.sh
@@ -24,16 +24,18 @@ test "refs/heads/main" = "$(git -C "$build_extra_dir" symbolic-ref HEAD)" ||
 die "Need the current branch in '$build_extra_dir' to be 'main'"
 
 tag_name="$(git -C "$git_git_dir" describe --match 'v[0-9]*' --exclude='*-[0-9]*' "$git_rev")-$(date +%Y%m%d%H%M%S)" &&
+tag_message="Snapshot build" &&
+release_note="Snapshot of $(git -C "$git_git_dir" show -s --pretty='tformat:%h (%s, %ad)' --date=short "$git_rev")" &&
+(cd "$build_extra_dir" && node ./add-release-note.js --commit feature "$release_note") &&
+display_version=${tag_name#v} &&
+ver=prerelease-${tag_name#v} &&
 
 mkdir -p "$artifacts_dir" &&
-echo "prerelease-${tag_name#v}" >"$artifacts_dir"/ver &&
-echo "${tag_name#v}" >"$artifacts_dir"/display_version &&
+echo "$ver" >"$artifacts_dir"/ver &&
+echo "$display_version" >"$artifacts_dir"/display_version &&
 echo "$tag_name" >"$artifacts_dir"/next_version &&
 
-git -C "$git_git_dir" tag $(test -z "$GPGKEY" || echo " -s") -m "Snapshot build" "$tag_name" "$git_rev" &&
+git -C "$git_git_dir" tag $(test -z "$GPGKEY" || echo " -s") -m "$tag_message" "$tag_name" "$git_rev" &&
 git -C "$git_git_dir" bundle create "$artifacts_dir"/git.bundle origin/main.."$tag_name" &&
-release_note=$(git -C "$git_git_dir" show -s  --pretty='tformat:%h (%s, %ad)' --date=short "$git_rev") &&
 
-cd "$build_extra_dir" &&
-node ./add-release-note.js --commit feature "Snapshot of $release_note" &&
-git bundle create "$artifacts_dir"/build-extra.bundle origin/main..main
+git -C "$build_extra_dir" bundle create "$artifacts_dir"/build-extra.bundle origin/main..main
