bb0137df87f355892652c16894985773610fb749
Johannes Schindelin <johannes.schindelin@gmx.de>
2023-03-02 08:54:40 +0100

    https-request: offer to upload files, too
    When an entire file needs to be uploaded, it is probably not a good idea
    to read the contents into memory and pass them as parameter. It is much
    better to open a stream and pipe it through.
    
    To support that, we allow to pass a file read stream instead of a string
    or an object.
    
    Sadly, we cannot use arbitrary read stream, it has to be a file read
    stream, because we need to know the file name and the file length.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- https-request.js
+++ https-request.js
@@ -6,11 +6,49 @@ module.exports = async (context, hostname, method, requestPath, body, headers) =
         Accept: 'application/json',
         ...headers || {}
     }
-    if (body) {
-        if (typeof body === 'object') body = JSON.stringify(body)
-        headers['Content-Type'] = 'application/json'
-        headers['Content-Length'] = body.length
+
+    let writeBody = req => {
+        req.end()
     }
+
+    if (body) {
+        const fs = require('fs')
+        if (body instanceof fs.ReadStream) {
+            const path = body.path
+            const stat = fs.statSync(path)
+            headers['Content-Length'] = stat.size
+            if (!headers['Content-Type']) {
+                const [fileExtension] = path.match(/\.[^.]+$/)
+                headers['Content-Type'] = {
+                    '.7z': 'application/zip',
+                    '.bz2': 'application/x-bzip2',
+                    '.deb': 'application/x-debian-package',
+                    '.exe': 'application/executable',
+                    '.gz': 'application/gzip',
+                    '.js': 'application/javascript',
+                    '.md': 'text/markdown',
+                    '.png': 'image/png',
+                    '.sh': 'text/x-shellscript',
+                    '.svg': 'image/svg+xml',
+                    '.txt': 'text/plain',
+                    '.xz': 'application/x-xz',
+                    '.zip': 'application/zip'
+                }[fileExtension] || 'application/octet-stream'
+            }
+            writeBody = req => {
+                body.pipe(req)
+            }
+        } else {
+            if (typeof body === 'object') body = JSON.stringify(body)
+            headers['Content-Type'] = 'application/json'
+            headers['Content-Length'] = body.length
+            writeBody = req => {
+                req.write(body)
+                req.end()
+            }
+        }
+    }
+
     const options = {
         port: 443,
         hostname: hostname || 'api.github.com',
@@ -83,8 +121,7 @@ module.exports = async (context, hostname, method, requestPath, body, headers) =
             })
 
             req.on('error', err => reject(err))
-            if (body) req.write(body)
-            req.end()
+            writeBody(req)
         } catch (e) {
             reject(e)
         }
