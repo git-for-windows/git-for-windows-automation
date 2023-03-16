65eb988f268e9b376d05624bdb7a23257d887c9e
Johannes Schindelin <johannes.schindelin@gmx.de>
2023-03-16 12:28:47 +0100

    repository-updates: merge provided bundle not only for `build-extra`
    When releasing Git for Windows v2.40.0, the `MINGW-packages` updates
    were not pushed. The reason was the faulty logic that cloned that
    repository, then _fetched_ from the bundle without merging, and pushed
    (which claimed that everything was up to date).
    
    Obviously, we need to merge the fetched ref!
    
    It gets a bit more complicated in the case of `MINGW-packages` because
    we are in a bare repository...
    
    So let's see if we can fast-forward, and if not, create a temporary
    worktree to perform the merge, hoping that it works without merge
    conflicts ;-)
    
    (In case of merge conflicts, the maintainer who triggered the release
    needs to download the bundle manually, merge the changes, resolve the
    conflicts, push to `main`, and then re-run the failed job.)
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- repository-updates.js
+++ repository-updates.js
@@ -20,6 +20,33 @@ const getWorkflowRunArtifact = async (context, token, owner, repo, workflowRunId
   await downloadAndUnZip(token, urls[name], name)
 }
 
+const mergeBundle = (gitDir, worktree, bundlePath, refName) => {
+  callGit(['--git-dir', gitDir, 'fetch', bundlePath, refName])
+  // If there is nothing to merge, return early
+  if (callGit(['--git-dir', gitDir, 'rev-list', '--count', `${refName}..FETCH_HEAD`]) === '0') return
+
+  if (worktree) {
+    callGit(['branch', '-M', refName], worktree)
+    callGit(['merge', '--no-edit', 'FETCH_HEAD'], worktree)
+  } else {
+    // If it fast-forwards, we do not need a worktree
+    if (callGit(['--git-dir', gitDir, 'rev-list', '--count', `FETCH_HEAD..${refName}`]) === '0') {
+      callGit(['--git-dir', gitDir, 'update-ref', `refs/heads/${refName}`, 'FETCH_HEAD'])
+    } else {
+      // Fine. We need a worktree. But we don't have one. So let's create one.
+      worktree = `${gitDir}/tmp-worktree`
+      callGit(['--git-dir', gitDir, 'worktree', 'add', '--no-checkout', worktree])
+      // No need for a full checkout, it's not as if we want to resolve any merge conflicts...
+      callGit(['sparse-checkout', 'set'], worktree)
+      callGit(['switch', '-d', refName], worktree)
+      // Perform the merge
+      callGit(['fetch', bundlePath, refName], worktree)
+      callGit(['merge', '--no-edit', 'FETCH_HEAD'], worktree)
+      callGit(['update-ref', `refs/heads/${refName}`, 'HEAD'], worktree)
+    }
+  }
+}
+
 const pushRepositoryUpdate = async (context, setSecret, appId, privateKey, owner, repo, refName, bundlePath) => {
   context.log(`Pushing updates to ${owner}/${repo}`)
 
@@ -31,13 +58,9 @@ const pushRepositoryUpdate = async (context, setSecret, appId, privateKey, owner
     `https://github.com/${owner}/${repo}`, repo
   ])
 
-  if (bundlePath) {
-    callGit(['--git-dir', gitDir, 'fetch', bundlePath, refName])
-  }
+  if (bundlePath) mergeBundle(gitDir, !bare && repo, bundlePath, refName)
 
   if (repo === 'build-extra') {
-    callGit(['branch', '-M', refName], repo)
-    callGit(['merge', '--no-edit', 'FETCH_HEAD'], repo)
     callProg('./download-stats.sh', ['--update'], repo)
     callGit(['commit', '-s', '-m', 'download-stats: new Git for Windows version', './download-stats.sh'], repo)
   } else if (repo === 'git-for-windows.github.io') {
@@ -74,6 +97,7 @@ const pushRepositoryUpdate = async (context, setSecret, appId, privateKey, owner
 }
 
 module.exports = {
+  mergeBundle,
   callGit,
   getWorkflowRunArtifact,
   pushRepositoryUpdate
