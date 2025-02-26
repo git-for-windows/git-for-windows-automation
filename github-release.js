20850d11cf76093f3a6876740b16f80b50de0bb8
Johannes Schindelin <johannes.schindelin@gmx.de>
2025-02-26 23:14:54 +0100

    github-release: only MinGit is built for i686, no other artifacts
    This is yet another step towards stopping to publish all i686 artifacts
    except MinGit, see https://github.com/git-for-windows/git/issues/5394.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- github-release.js
+++ github-release.js
@@ -209,6 +209,7 @@ const getGitArtifacts = async (
     const urls = await getWorkflowRunArtifactsURLs(context, token, owner, repo, workflowRunId)
     for (const artifact of artifacts) {
       if (architecture.name === 'aarch64' && artifact.name === 'mingit-busybox') continue
+      if (architecture.name === 'i686' && !artifact.name.startsWith('mingit')) continue
       const name = `${artifact.name}-${architecture.name}`
       context.log(`Downloading ${name}`)
       await downloadAndUnZip(token, urls[name], name)
